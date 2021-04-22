d7d92e18cc4bdba377f66b9a65a3ec6fe4c04165
JMR-Mejia <chucho4720013@gmail.com>
2021-04-22 00:13:54 -0500

    Introducción a Fetch y API de Coincap

--- src/components/PxAssetsTable.vue
+++ src/components/PxAssetsTable.vue
@@ -1,32 +1,45 @@
 <template>
-  <table>
-    <thead>
-      <tr class="bg-gray-100 border-b-2 border-gray-400">
-        <th></th>
-        <th>
-          <span>Ranking</span>
-        </th>
-        <th>Nombre</th>
-        <th>Precio</th>
-        <th>Cap. de Mercado</th>
-        <th>Variación 24hs</th>
-        <td class="hidden sm:block"></td>
-      </tr>
-    </thead>
-    <tbody>
-      <tr
-        class="border-b border-gray-200 hover:bg-gray-100 hover:bg-orange-100"
-      >
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td class="hidden sm:block"></td>
-      </tr>
-    </tbody>
-  </table>
+  <div>
+    <table>
+      <thead>
+        <tr class="bg-gray-100 border-b-2 border-gray-400">
+          <th></th>
+          <th>
+            <span>Ranking</span>
+          </th>
+          <th>Nombre</th>
+          <th>Precio</th>
+          <th>Cap. de Mercado</th>
+          <th>Variación 24hs</th>
+          <td class="hidden sm:block"></td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="a in assets"
+          :key="a.id"
+          class="border-b border-gray-200 hover:bg-gray-100 hover:bg-orange-100"
+        >
+          <td>
+            <img
+              :src="`https://static.coincap.io/assets/icons/${a.symbol.toLowerCase()}@2x.png`"
+              alt="a.name"
+            />
+          </td>
+          <td>
+            <b>#{{ a.rank }}</b>
+          </td>
+          <td>
+            {{ a.name }}
+          </td>
+          <td> {{ a.priceUsd }}</td>
+          <td>{{ a.marketCapUsd }}</td>
+          <td>{{ a.changePercent24Hr }}</td>
+          <td class="hidden sm:block"></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
